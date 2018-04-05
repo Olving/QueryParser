9064b0974fa65b9c09abf597421b5ede461781eb
JosepPC <JosepPC@HCES5CG54924L4>
2018-04-05 13:37:22 +0200

    INIT Commit

---
+++ QueryParser/src/main/java/com/dxc/esfds/dojo/queryparser/QueryParser.java
@@ -0,0 +1,5 @@
+package com.dxc.esfds.dojo.queryparser;
+
+public class QueryParser {
+
+}
---
+++ QueryParser/src/test/java/com/dxc/esfds/dojo/queryparser/QueryParserTests.java
@@ -0,0 +1,17 @@
+package com.dxc.esfds.dojo.queryparser;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+
+
+
+
+public class QueryParserTests {
+	@Test
+	public void test() {
+		assertThat(true, is(true));
+	}
+}
